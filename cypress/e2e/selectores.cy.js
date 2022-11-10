8b8b0a0a2966bd5a6bb8c5da36de284c7fec505b
Fabrizio Otranto <Fabryotranto@gmail.com>
2022-11-09 21:39:33 -0300

    clase-2

--- cypress/e2e/selectores.cy.js
+++ cypress/e2e/selectores.cy.js
@@ -1,6 +1,6 @@
 describe("Selectores", () => {
 
-    it("Deberia encontrar los elementos utilizando cssSelectors", () => {
+    it.skip("Deberia encontrar los elementos utilizando cssSelectors", () => {
         cy.visit('')
         cy.get('button') // por tagname
         cy.get('[name="user"]') // por atributo
@@ -14,6 +14,33 @@ describe("Selectores", () => {
         cy.get('[class*="password"]')  //encontramos valor de forma parcial en cualquier lugar
         cy.get('[name^="pa"]')//encontramos valor de forma parcial que esta al comienzo
         cy.get('[name$="ss"]')//encontramos valor de forma parcial que esta al final
+
+    })
+
+    it('Deberia encontrar el input value male utilizando find', () =>{
+        cy.visit('')
+        cy.get('fieldset').find('input[value="Male"]')
+    })
+
+    it('Deberia encontrar el legend utilizando children', () =>{
+        cy.visit('')
+        cy.get('fieldset').children('legend')
+    })
+
+    it('Deberia encontrar el fieldset utilizando parent', () =>{
+        cy.visit('')
+        cy.get('legend').parent('fieldset')
+    })
+
+    it('Deberia encontrar el div  utilizando siblings', () =>{
+        cy.visit('')
+        cy.get('legend').siblings('div')
+    })
+
+    it('Deberia encontrar el button register utilizando contains', () =>{
+        cy.visit('')
+        cy.contains("register", {matchCase:false}) //sin respetar mayusuclas
+        cy.contains("Register") // respetando mayusuclas
 
     })
 })
