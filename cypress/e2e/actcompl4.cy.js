6e0e2f93b37e652ec44a2ba79beebb4baf2aecc9
Fabrizio Otranto <Fabryotranto@gmail.com>
2022-12-05 11:58:18 -0300

    adding tag

--- cypress/e2e/actcompl4.cy.js
+++ cypress/e2e/actcompl4.cy.js
@@ -16,7 +16,7 @@ describe('Actividad complementaria 4', () => {
         cy.get('#registertoggle').dblclick();
     })
 
-    it('Deberia validar un mensaje de error al colocar un usuario inexistente', () => {
+    it('Deberia validar un mensaje de error al colocar un usuario inexistente',{tags: "@T1cce938c"}, () => {
         cy.get('#user').type(act4Fixture.usuario);
         cy.get('#pass').type(act4Fixture.contraseña);
         cy.get('#submitForm').click();
