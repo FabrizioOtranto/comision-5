3f1c3b9fb7837cf501a90f6a2d9a1b22dd1dc147
erialexa93 <erialexa93@gmail.com>
2022-12-12 17:35:15 -0500

    Desafio 4

---
+++ cypress/support/Erika_Pinto.js
@@ -0,0 +1,14 @@
+export const REGISTRO ={
+    USER: "erialexa"+ Math.floor(Math.random() * 1000),
+    PASS: "123456!",
+    GENDER: "Female",
+    DAY: "01",
+    MONTH: "September",
+    YEAR: "1993",
+}
+
+export const API ={
+    URL_POST_REGISTER: "https://pushing-it.onrender.com/api/register",
+    URL_POST_LOGIN: "https://pushing-it.onrender.com/api/login",
+    URL_DELETE_USER: "https://pushing-it.onrender.com/api/deleteuser/"
+}
