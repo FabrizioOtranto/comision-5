b7d9fd3af30b1e8f13c0024284c669df554e5477
TorreCristianHernan <1987cristiantorre@gmail.com>
2022-12-13 00:33:49 -0300

    desafio4

---
+++ cypress/e2e/1-getting-started/CristianTorre.cy.js
@@ -0,0 +1,64 @@
+/// <reference types="cypress" />
+
+describe('Torre Critian Api', () => {
+
+    
+    
+    it('Registro ingreso y elimino usuario', () => {
+        const username ='usuariio'
+        const password= '123456!'
+        const gender='Male'
+        const day='5'
+        const month='October'
+        const year= '1988'
+
+        cy.request({
+            method: "POST",
+            url: "https://pushing-it.onrender.com/api/register",
+            body:{
+                username: username,
+                password: password,
+                gender:gender,
+                day: day,
+                month: month,
+                year: year
+            }
+        }).then( respuesta => {
+            expect(respuesta.status).is.equal(200)
+            expect(respuesta.body.newUser.username).is.equal(username)
+        
+        });
+            
+            cy.request({
+                method: "POST",
+                url:  "https://pushing-it.onrender.com/api/login",
+                body:{
+                    username: username,
+                    password: password,
+    
+                }
+     
+            }).then( respuesta => {
+                window.localStorage.setItem('token',respuesta.body.token)
+                window.localStorage.setItem('user',respuesta.body.user.username)
+
+            })
+            cy.request({
+                method:"DELETE",
+                url: `https://pushing-it.onrender.com/api/deleteuser/${username}`,
+            }).then((respuesta)=>{
+                    cy.log(respuesta)
+               
+                
+            
+            })
+            
+        
+    })
+})
+
+
+
+
+
+    
---
+++ cypress/e2e/1-getting-started/Desafio5.cy.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+describe('Torre Critian Api', () => {
+
+    
+    
+    it('Registro ingreso y elimino usuario', () => {
+        const username ='usuariaaoo'
+        const password= '123456!'
+        const gender='Male'
+        const day='5'
+        const month='October'
+        const year= '1988'
+
+        cy.request({
+            method: "POST",
+            url: "https://pushing-it.onrender.com/api/register",
+            body:{
+                username: username,
+                password: password,
+                gender:gender,
+                day: day,
+                month: month,
+                year: year
+            }
+        }).then( respuesta => {
+            expect(respuesta.status).is.equal(200)
+            expect(respuesta.body.newUser.username).is.equal(username)
+
+        });
+
+ 
+        cy.request({
+            method: "POST",
+            url:  "https://pushing-it.onrender.com/api/login",
+            body:{
+                username: username,
+                password: password,
+
+            }
+ 
+        }).then( respuesta => {
+            cy.log(respuesta.body)
+            window.localStorage.setItem('token',respuesta.body.token)
+            window.localStorage.setItem('user',respuesta.body.username)
+
+        })
+    })
+})
